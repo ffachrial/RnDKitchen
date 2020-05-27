0d2a98aab3140e91915fef4af458975132f63579
Firman Fachrial <fachrial@yahoo.com>
2020-05-27 17:41:33 +0700

    Add Search function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime";
 import './styles/style.css';
 import './script/component/app-header.js';
 import main from './script/view/main.js';
--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,3 +1,29 @@
-const main = () => {};
+import '../component/meal-list.js';
+import '../component/search-item.js';
+import DataSource from '../data/data-source.js';
+
+const main = () => {
+    const searchElement = document.querySelector("search-item");
+    const mealListElement = document.querySelector("meal-List");
+
+    const onButtonSearchClicked = async () => {
+        try{
+            const result = await DataSource.searchMeal(searchElement.value);
+            renderResult(result);
+        } catch (message) {
+            fallbackResult(message)
+        }
+    };
+
+    const renderResult = results => {
+        mealListElement.meals = results;
+    };
+
+    const fallbackResult = message => {
+        mealListElement.renderError(message);
+    };
+
+    searchElement.clickEvent = onButtonSearchClicked;
+};
 
 export default main;
